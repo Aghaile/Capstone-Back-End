13fdb98049c83c7b50ce9cd9644d6bfd6910e6e0
Adah Hailemariam <aghailemariam@gmail.com>
2022-01-27 14:24:44 -0800

    human and pet models

---
+++ app/models/human.py
@@ -0,0 +1,10 @@
+from app import db
+class Human(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username=db.Column(db.String)
+    zipcode=db.Column(db.String)
+
+    def convert_human_to_dict(self):
+        return { "id": self.id,
+                "username": self.username,
+                "zipcode": self.zipcode}
---
+++ app/models/pet.py
@@ -0,0 +1,10 @@
+from app import db
+class Pet(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name=db.Column(db.String)
+    zipcode=db.Column(db.String)
+
+    def convert_pet_to_dict(self):
+        return { "id": self.id,
+                "name": self.name,
+                "zipcode": self.zipcode}
