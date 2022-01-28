f9c727cc82f4c029a3bec95c09dff302fa133463
Ro Peeples <rogpeeples@gmail.com>
2022-01-28 12:30:21 -0500

    Merge branch 'main' of https://github.com/Aghaile/Capstone-Back-End

--- app/models/pet.py
+++ app/models/pet.py
@@ -2,7 +2,7 @@ from app import db
 class Pet(db.Model):
     id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     name=db.Column(db.String)
-    zipcode=db.Column(db.String)
+    zipcode=db.Column(db.Integer)
 
     def convert_pet_to_dict(self):
         return { "id": self.id,
