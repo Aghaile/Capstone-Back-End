2848af4de83351b6333d476ad43877dd3497e876
Ro Peeples <rogpeeples@gmail.com>
2022-02-02 14:12:56 -0500

    CRUD for pets

--- app/routes.py
+++ app/routes.py
@@ -15,59 +15,102 @@ def create_a_profile():
         return make_response({"details": "Request body must include zipcode."}, 400)
     elif request_body['phone_number'] == "" or 'phone_number' not in request_body:
         return make_response({"details": "Request body must include a phone number."}, 400)
-        # taking info fr request_body and converting it to new Board object
     new_profile = Pet(
         name = request_body["name"],
         zipcode = request_body["zipcode"],
         phone_number = request_body["phone_number"]
         )
-    # committing changes to db
     db.session.add(new_profile)
     db.session.commit()
     confirmed = new_profile.convert_pet_to_dict()
     return jsonify(confirmed), 201
 
-@pet_bp.route("/pet/<pet_id>", methods=["GET", "PATCH", "DELETE"]) #for the welcome page
+@pet_bp.route("/<pet_id>", methods=["GET", "PATCH", "DELETE"]) #for the welcome page
 def find_a_profile(pet_id):
-    # either get Card back or None, card here is an object
+    if not pet_id.isnumeric():
+         return jsonify(None), 400
+
     pet = Pet.query.get(pet_id)
-    if pet is None:
-        return make_response({"message": f"{pet.name} was not found"}, 404)
 
-    # PATCH will change just one part of the record, not the whole record
+    if not pet:
+        return jsonify({"message": f"Pet {pet_id} was not found"}), 404
+    
+    if request.method == "GET":
+        return jsonify(pet.convert_pet_to_dict()), 200
 
-    if request.method == "PATCH":
-        form_data = request.get_json()
-        if "bio" in form_data:
-            pet.bio = form_data["bio"]
-        elif "zipcode" in form_data:
-            pet.zipcode = form_data["zipcode"]
-        elif "phone_number" in form_data:
-            pet.phone_number = ["phone_number"]
-            db.session.commit()
-            return make_response({}, 200)
+    elif request.method == "PATCH":
+        pet_update_request_body = request.get_json()
+        pet.name = pet_update_request_body["name"],
+        pet.bio = pet_update_request_body["bio"],
+        pet.age = pet_update_request_body["age"],
+        pet.gender = pet_update_request_body["gender"],
+        pet.zipcode = pet_update_request_body["zipcode"],
+        pet.phone_number = pet_update_request_body["phone_number"]
 
-    if request.method == "DELETE":
-        # query db for specific card object by the card id
-        # if Card.query.filter_by(id=card_id):
+        db.session.commit()
+
+        updated_pet_response = pet.convert_pet_to_dict()
+
+        return jsonify(updated_pet_response), 200
+
+    elif request.method == "DELETE":
         db.session.delete(pet)
         db.session.commit()
-        return make_response({'message': f"{pet.name}'s profile was deleted"}, 200)
+        pet_delete_response = pet.convert_pet_to_dict()
+
+        print("********", pet_delete_response), 200
+        return jsonify(pet_delete_response), 200
 
 
-@pet_bp.route("/pet/<pet_id>", methods=["GET"])
-def get_all_friendships_for_one_pet(pet_id):
-    if not Pet.query.get(pet_id):
-        return jsonify({"details": f"{pet_id} was not found"}), 404
+# @friendship_bp.route("/<pet_id>", methods=["GET"])
+# def get_all_friendships_for_one_pet(pet_id):
+#     if not Pet.query.get(pet_id):
+#         return jsonify({"details": f"{pet_id} was not found"}), 404
 
-    sort_query = request.args.get("sort")
-    if sort_query == "potential_pals":
-        pals = Friendship.query.get(Friendship.friendship_id).filter_by(potential_pals=True).all()
-    if sort_query == "pending_pals":
-        pals = Friendship.query.get(Friendship.friendship_id).filter_by(pending_pals=True).all()
-    if sort_query == "pawsitively_pals":
-        pals = Friendship.query.get(Friendship.friendship_id).filter_by(pawsitively_pals=True).all()
-    else:
-        # no order specified
-        pals = Friendship.query.get(pet_id).pals
-    return jsonify([pals]), 200
+#     sort_query = request.args.get("sort")
+#     if sort_query == "potential_pals":
+#         pals = Friendship.query.get(Friendship.friendship_id).filter_by(potential_pals=True).all()
+#     if sort_query == "pending_pals":
+#         pals = Friendship.query.get(Friendship.friendship_id).filter_by(pending_pals=True).all()
+#     if sort_query == "pawsitively_pals":
+#         pals = Friendship.query.get(Friendship.friendship_id).filter_by(pawsitively_pals=True).all()
+#     else:
+#         # no order specified
+#         pals = Friendship.query.get(pet_id).pals
+#     return jsonify([pals]), 200
+
+# @friendship_bp.route("/add", methods=["POST"])
+# def add_new_pal():
+#     pal_request_body = request.get_json()
+    
+#     if "pet_id" not in pal_request_body or "pal_id" not in pal_request_body:
+#         return jsonify(None), 400
+
+#     your_pet = Pet.query.get(pal_request_body["pet_id"])
+#     if your_pet is None:
+#         return jsonify(None), 404
+
+#     friends_pet = Pet.query.get(pal_request_body["pal_id"])
+#     if friends_pet is None:
+#         return jsonify(None), 404
+    
+#     if Friendship.your_pet.pawsitively_pals == True:
+#         return jsonify({"message": "You are already pals!"}), 400
+    
+#     db.session.query(Friendship).filter(Friendship.pal_id == your_pet).update({"pawsitively_pals": (True)})
+
+#     new_pal = Friendship(
+#         pet_id=your_pet,
+#     )
+
+#     db.session.add(new_pal)
+#     db.session.commit() 
+    
+#     friend_confirmed = {
+#         "pal_id": pal_id.pet_id,
+#         "video_id": new_rental.video_id,
+#         "due_date": new_rental.due_date,
+#         "videos_checked_out_count": videos_checked_out,
+#         "available_inventory": video.total_inventory
+#         }
+#     return jsonify(rental_receipt), 200
