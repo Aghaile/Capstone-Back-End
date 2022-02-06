5e5b061ffc2508441115226dd6d2cc3f1b09bf33
Ro Peeples <rogpeeples@gmail.com>
2022-02-05 17:08:36 -0700

    updated pet and friendship models

--- app/models/friendship.py
+++ app/models/friendship.py
@@ -2,5 +2,5 @@ from app import db
 class Friendship(db.Model):
     friendship_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     pet=db.relationship("Pet", backref="friendships")
-    pet_id=db.Column(db.Integer, db.ForeignKey("pet.pet_id"), nullable=False)
+    pet_id=db.Column(db.Integer, db.ForeignKey("pet.id"), nullable=False)
     pals=db.Column(db.Boolean, default=False)
